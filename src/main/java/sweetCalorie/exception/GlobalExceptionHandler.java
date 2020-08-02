46620835859a913f571fc4168eb396732f8f1567
Evuns <m_ateva@abv.bg>
2020-08-02 22:19:27 +0300

    Exception handle

---
+++ src/main/java/sweetCalorie/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package sweetCalorie.exception;
+
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//    @ExceptionHandler(Throwable.class)
+//    public ModelAndView handleException(Throwable e) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.addObject(e.getCause());
+//        modelAndView.setViewName("error");
+//        return modelAndView;
+//    }
+//}
